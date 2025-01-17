d18d7d4ef40d45d261dcf03430449feba42e24aa
ramonezorrilla <ramoenzorrilla@gmail.com>
2025-01-17 18:42:38 -0400

    Add project files.

---
+++ clase_crear_objeto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Persona
+  {
+
+    public string Nombre;
+    public int Edad;
+
+    public void Saludar()
+
+    { 
+    Console.WriteLine($"Hola, Soy {Nombre} y Tengo {Edad} años.");
+    }
+
+    }
+
+class Program
+{
+
+    static void Main()
+    {
+        Persona persona1 = new Persona();
+        persona1.Nombre = "Carlos";
+        persona1.Edad = 30;
+        persona1.Saludar();
+    
+    }
+
+}
